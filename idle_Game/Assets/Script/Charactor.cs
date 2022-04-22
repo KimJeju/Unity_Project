040d1268854229827e4775565caaa0a93b6c4c0b
KunHwi kim <80661467+KimJeju@users.noreply.github.com>
2022-04-22 15:30:54 +0900

    방치형 RPG 제작 :: 캐릭터 스크립트 작성 중

---
+++ idle_Game/Assets/Script/Charactor.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;                  //BigInteger를 사용하기 위해서 선언
+using System.Numerics;         // Numerics 네임스페이스를 선언해야 BigInterger를 사용할 수 있다.
+
+public class Charactor : MonoBehaviour
+{
+   
+  /*
+  BigInterger를 선언해준 이유는 게임 후반 Player의 체력과 같은 수치가 매우 큰값이 되었을 시 int형으로 담을 수 없기 때문
+  */
+  public BigInteger Hp;  // Player 체력
+  
+  public BigInteger HpMax;  // Player가 가지는 최대 HP
+
+  public BigInteger Damege; // Player가 받은 데미지
+
+  public float Attack_Speed; //플레이어 공격속도.
+
+  public void init()  // 함수 초기화
+  {
+      HpMax = 100;
+      Hp = HpMax;
+      Damege = 10;
+      Attack_Speed = 1.0f;
+  } // 초기화를 위한 함수
+
+  public void GetHp(BigInteger Hp) // 현재 Hp가 HpMax보다 커지게된다면 HpMax와 동일하게 세팅해준다.
+  {
+        Hp += Hp; 
+
+        if(Hp > HpMax)
+        {
+            Hp = HpMax;
+        }
+  } 
+
+  public void getDamege(BigInteger damege) //받는 데미지를 표현하기 위한 메서드
+  {
+      Hp -= damege;
+
+      if(Hp <= 0) //플레이어 체력이 0이되면 Dead() 메서드를 호출해준다.
+      {
+          Dead();
+      }
+  }
+
+  
+  public void Dead() 
+  {
+      Debug.Log("DEAD");
+  }
+
+
+}
